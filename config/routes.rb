8735647d4458354c08014dc29cb72d0e9cba70a5
Rodrigo Alba <rodrigoqalba@gmail.com>
2023-05-09 09:55:58 -0400

    overall commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  # root 'home#index'
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index, :show]
+      resources :companies, only: [:index, :show]
+      resources :reviews
+      post '/signup', to: 'users#create'
+		  get '/me', to: 'users#show'
+
+	    post '/login', to: 'sessions#create'
+		  delete '/logout', to: 'sessions#destroy'
+    end
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
+end
